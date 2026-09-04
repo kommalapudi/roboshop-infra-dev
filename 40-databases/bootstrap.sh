fc6f26ac5c9722377099aaff3444cc62c236f807
chaitanya <kommalkrishna@gmail.com>
2026-09-04 13:36:36 +0530

    update bootstrap file

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+component=$1
 dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/kommalapudi/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+ansible-playbook -e component=${component} reboshop.yml
+
