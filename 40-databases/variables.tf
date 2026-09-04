55be52c7ab50a780ce1cbc5459e3784f9e5a6b97
chaitanya <kommalkrishna@gmail.com>
2026-09-04 15:48:04 +0530

    redis db config

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z004782564HOFLM6URBO"
+}
+
+variable "domain_name" {
+  default = "kcdevops.online"
+}
