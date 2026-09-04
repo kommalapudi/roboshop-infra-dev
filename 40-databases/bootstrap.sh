a1921d81dd37912f4c836c50b84d2fbe74090fef
chaitanya <kommalkrishna@gmail.com>
2026-09-04 16:37:13 +0530

    udpate bootstrap script

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -4,6 +4,7 @@ component=$1
 dnf install ansible -y
 
 cd /home/ec2-user
+sudo rm -rf /home/ec2-user/ansible-roboshop-roles-tf
 git clone https://github.com/kommalapudi/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
