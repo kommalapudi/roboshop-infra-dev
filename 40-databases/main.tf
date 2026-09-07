68b67c9892712ab684106f7ceca5272a67fdd2e7
chaitanya <kommalkrishna@gmail.com>
2026-09-07 19:26:44 +0530

    update mysql infra setup

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "mongodb" {
 
 
 
-resource "terraform_data" "bootstrap" {
+resource "terraform_data" "mongodb" {
   triggers_replace = [
     aws_instance.mongodb.id
   ]
@@ -78,4 +78,46 @@ resource "terraform_data" "bootstrap_redis" {
     ]
   }
 }
+
+# MySQL
+
+resource "aws_instance" "mysql" {
+  ami          = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id     = local.database_subnet_id
+  vpc_security_group_ids = [local.mysql_sg_id]
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-mysql"
+  })
+}
+
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+  
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password    = "DevOps321"
+    host        = aws_instance.mysql.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh mysql"
+    ]
+  }
+}
+
+
 
