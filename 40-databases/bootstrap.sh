054a03908d3457fd434d44c56f503f6c97300aa0
chaitanya <kommalkrishna@gmail.com>
2026-09-10 14:53:10 +0530

    environment -> env

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -10,5 +10,5 @@ git clone https://github.com/kommalapudi/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
 
